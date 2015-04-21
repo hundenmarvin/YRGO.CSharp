e622fc0b7c6e78f0c2ec988499cbf161817be54c
John Strömblom <john@esverse.com>
2015-04-21 16:30:40 +0200

    Added key for chapter six.

---
+++ YRGO.CS.6/Cat.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YRGO.CS._6
+{
+    class Cat
+    {
+        public string Name { get; set; }
+        private bool IsZombie { get; set; }
+
+        public Cat(string name)
+        {
+            Name = name;
+        }
+
+        public Cat(string name, bool isZombie)
+        {
+            Name = name;
+            IsZombie = isZombie;
+        }
+
+        public void Talk()
+        {
+            if(IsZombie)
+                Console.WriteLine("<{0}> Braaains!", Name);
+            else
+                Console.WriteLine("<{0}> Mjaooo!", Name);
+        }
+
+        public void Talk(string text)
+        {
+            if (IsZombie || String.IsNullOrEmpty(text))
+                Talk();
+            else
+                Console.WriteLine("<{0}> {1}", Name, text);
+        }
+
+        public void Eat(Cat cat)
+        {
+            if (IsZombie)
+            {
+                Console.WriteLine("<{0}> Nom nom nom!", Name);
+                cat.IsZombie = true;
+            }
+            else
+            {
+                Console.WriteLine("<{0}> I'm not a cannibal! I will NOT eat {1}!", Name, cat.Name);
+            }
+        }
+    }
+}
---
+++ YRGO.CS.6/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YRGO.CS._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //One();
+            //Two();
+            //Three();
+            Four();
+        }
+
+        static void One()
+        {
+            var hugo = new Cat("Hugo");
+            hugo.Talk();
+        }
+
+        static void Two()
+        {
+            var hugo = new Cat("Hugo", true);
+            hugo.Talk();
+        }
+
+        static void Three()
+        {
+            var hugo = new Cat("Hugo", true);
+            var fisker = new Cat("Fisker");
+
+            hugo.Talk();
+            fisker.Talk("Need kebab!");
+
+            hugo.Talk("Ooo, kebab!");
+            fisker.Talk();
+        }
+
+        static void Four()
+        {
+            var hugo = new Cat("Hugo");
+            var fisker = new Cat("Fisker");
+
+            hugo.Eat(fisker);
+
+            // ------------------
+
+            var baltazar = new Cat("Baltazar", true);
+
+            baltazar.Talk();
+            fisker.Talk();
+
+            baltazar.Eat(fisker);
+            fisker.Talk();
+        }
+    }
+}
