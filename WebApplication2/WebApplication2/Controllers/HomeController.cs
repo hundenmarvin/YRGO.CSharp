327765119a742c888af553eda86ca0b855a76e54
John Strömblom <john@esverse.com>
2015-05-07 23:24:08 +0200

    Added mvc application. Updated git ignore.

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+            var test = new ClassLibrary1.Class1();
+            var model = new ContactViewModel()
+            {
+                Name = "John",
+                Phone = "133 - 71 10 10"
+            };
+
+            return View(model);
+        }
+
+        public ActionResult ContactForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ContactForm(ContactFormModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                //ViewBag.Message = "Du nu missade du att fylla i något!";
+                return View();
+            }
+
+            ViewBag.Message = "Vi kommer höra av oss snarast!";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Test(string username, string password)
+        {
+            if(username == "kebab" && password == "svarv")
+                return Content("Woohooo!");
+
+            return Content("Phaaail!");
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/ContactFormModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class ContactFormModel
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Fyll i en email ditt stolpskott!")]
+        public string Email { get; set; }
+
+        public string Message { get; set; }
+    }
+}
